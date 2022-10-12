072393b6431d27a8a39627e90df26af4aa58b1c2
programmermohaimin <dev.mohaimin@gmail.com>
2022-10-12 08:43:32 +0600

    cerate modern app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import { useState } from 'react'
+import { close, logo, menu } from '../assets'
+import {navLinks} from '../constants'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='w-full flex justify-between items-center navbar py-6' >
+      <img src={logo} alt='Mohaimin Logo' className='w-[124px] h-[32px]'/>
+    </nav>
   )
 }
 
